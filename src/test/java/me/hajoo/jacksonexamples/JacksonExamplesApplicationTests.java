863e0af8983db69ea1af067b07a76b0e21b17c19
joohyun ha <mylifeforcoding@gmail.com>
2022-05-26 01:49:49 +0900

    init

---
+++ src/test/java/me/hajoo/jacksonexamples/JacksonExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hajoo.jacksonexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
